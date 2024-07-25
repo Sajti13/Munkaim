9728aaab7f713130cf3be238594f7d5e2e649007
Sajti13 <ajtaisandor13@gmail.com.com>
2024-07-25 16:45:54 +0200

    Frontend és Webfejlesztés

--- 20210114Feladatsor/feladat01.js
+++
@@ -1,5 +0,0 @@
-document.write("Nagy Ferenc");
-document.write("<br>AllTeam");
-document.write("<br>HTML: 100");
-document.write("<br>CSS: 95");
-document.write("<br>JavaScript: 95");
--- 20210114Feladatsor/feladat02.js
+++
@@ -1,5 +0,0 @@
-let szam=Number(prompt("Add meg a hatványozandó számot"));
-let hatvany=Number(prompt("Add meg a hatványozás mértékét"));
-//let eredmeny=szam**hatvany;
-let eredmeny=Math.pow(szam, hatvany);
-document.write(`A ${szam}<sup>${hatvany}</sup> eredménye: ${eredmeny}`);
